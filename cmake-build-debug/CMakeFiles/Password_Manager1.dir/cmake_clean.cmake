1d62fb340aeb0ef59fc6a26d7de6b31da6103a3c
kaluxikS <salamonskip1@gmail.com>
2023-06-02 10:27:39 +0200

    Finished.

---
+++ cmake-build-debug/CMakeFiles/Password_Manager1.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Password_Manager1.dir/PasswordManager.cpp.obj"
+  "CMakeFiles/Password_Manager1.dir/PasswordManager.cpp.obj.d"
+  "CMakeFiles/Password_Manager1.dir/main.cpp.obj"
+  "CMakeFiles/Password_Manager1.dir/main.cpp.obj.d"
+  "Password_Manager1.exe"
+  "Password_Manager1.exe.manifest"
+  "Password_Manager1.pdb"
+  "libPassword_Manager1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Password_Manager1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Password_Manager1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Password_Manager1.
+# This may be replaced when dependencies are built.
